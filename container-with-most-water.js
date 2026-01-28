166e04818b32b5459b39d3573dc45cad106d8646
Jack Crane <jack@jackcrane.rocks>
2026-01-27 22:10:28 -0600

    Faster, passes all test cases

--- container-with-most-water.js
+++ container-with-most-water.js
@@ -6,24 +6,24 @@
  */
 var maxArea = function (heights) {
   let max = 0;
-  for (let h1idx = 0; h1idx < heights.length; h1idx++) {
-    let height1 = heights[h1idx];
-    for (let h2idx = 0; h2idx < heights.length; h2idx++) {
-      if (h1idx === h2idx) continue;
-      let height2 = heights[h2idx];
 
-      let width = h2idx - h1idx;
-      let height = Math.min(height1, height2);
+  let l = 0;
+  let r = heights.length - 1;
 
-      let area = width * height;
-      if (area > max) {
-        max = area;
-      }
+  while (l < r) {
+    const width = r - l;
+    const height = Math.min(heights[l], heights[r]);
+    const area = width * height;
+    max = Math.max(area, max);
+    if (heights[l] < heights[r]) {
+      l++;
+    } else {
+      r--;
     }
   }
 
   return max;
 };
 
-console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
 // console.log(maxArea([4, 3, 2, 1, 4]));
